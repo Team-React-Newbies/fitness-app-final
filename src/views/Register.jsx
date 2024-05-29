1024a647283b5466615356100814725ca94147be
zhelyazkovse <zhelyazkovse@gmail.com>
2024-05-29 13:54:25 +0300

    register reworked with MUI

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,10 +1,14 @@
 import { useContext, useState, useEffect } from "react";
-//import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-//import { ref, set, getDatabase, get } from "firebase/database";
 import { AppContext } from "../context/AppContext.jsx";
 import { useNavigate } from "react-router-dom";
 import { createUserHandle, getUserByHandle } from "../services/users.service";
 import { registerUser } from "../services/auth.service";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 export default function Register() {
     const [form, setForm] = useState({
@@ -14,8 +18,6 @@ export default function Register() {
     });
     const { user, setAppState } = useContext(AppContext);
     const navigate = useNavigate();
-    // const auth = getAuth();
-    // const database = getDatabase();
 
     useEffect(() => {
         if (user) {
@@ -30,64 +32,103 @@ export default function Register() {
         }));
     };
 
-    // const register = async () => {
-    //     if (!form.username || !form.email || !form.password) {
-    //         alert("Please fill in all fields.");
-    //         return;
-    //     }
-
-    //     try {
-    //         const usernameRef = ref(database, `users/${form.username}`);
-    //         const usernameSnap = await get(usernameRef);
-    //         if (usernameSnap.exists()) {
-    //             alert('User with this username already exists!');
-    //             return;
-    //         }
-
-    //         const credential = await createUserWithEmailAndPassword(auth, form.email, form.password);
-    //         const userRef = ref(database, `users/${form.username}`);
-    //         await set(userRef, {
-    //             username: form.username,
-    //             uid: credential.user.uid,
-    //             email: form.email,
-    //             createdOn: new Date().toISOString()
-    //         });
-    //         setAppState({ user: credential.user, userData: { username: form.username, email: form.email } });
-    //         navigate('/');
-    //     } catch (error) {
-    //         console.error("Registration error:", error.message);
-    //         alert(error.message);
-    //     }
-    // };
-
-    const register = async() => {
-    // TODO: validate form data
-    try {
-      const user = await getUserByHandle(form.username);
-      if (user.exists()) {
-        return console.log('User with this username already exists!');
-      }
-      const credential = await registerUser(form.email, form.password);
-      await createUserHandle(form.username, credential.user.uid, credential.user.email);
-      setAppState({ user: credential.user, userData: null });
-      navigate('/');
-    } catch (error) {
-      if (error.message.includes('auth/email-already-in-use')) {
-        console.log('User has already been registered!');
-      }
-    }
-  };
+    const register = async () => {
+        // TODO: validate form data
+        try {
+            const user = await getUserByHandle(form.username);
+            if (user.exists()) {
+                return console.log('User with this username already exists!');
+            }
+            const credential = await registerUser(form.email, form.password);
+            await createUserHandle(form.username, credential.user.uid, credential.user.email);
+            setAppState({ user: credential.user, userData: null });
+            navigate('/');
+        } catch (error) {
+            if (error.message.includes('auth/email-already-in-use')) {
+                console.log('User has already been registered!');
+            }
+        }
+    };
 
     return (
-        <div>
-            <h1>Register</h1>
-            <label htmlFor="username">Username:</label>
-            <input value={form.username} onChange={updateForm('username')} type="text" name="username" id="username" /><br />
-            <label htmlFor="email">Email:</label>
-            <input value={form.email} onChange={updateForm('email')} type="email" name="email" id="email" /><br />
-            <label htmlFor="password">Password:</label>
-            <input value={form.password} onChange={updateForm('password')} type="password" name="password" id="password" /><br /><br />
-            <button onClick={register}>Register</button>
-        </div>
+        <Container maxWidth="sm">
+            <Paper elevation={3} sx={{ padding: 4, marginTop: 8 }}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Register
+                    </Typography>
+                    <Box component="form" sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                            value={form.username}
+                            onChange={updateForm('username')}
+                            InputProps={{
+                                style: { color: 'black' },
+                            }}
+                            InputLabelProps={{
+                                style: { color: 'grey' },
+                            }}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            value={form.email}
+                            onChange={updateForm('email')}
+                            InputProps={{
+                                style: { color: 'black' },
+                            }}
+                            InputLabelProps={{
+                                style: { color: 'grey' },
+                            }}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={form.password}
+                            onChange={updateForm('password')}
+                            InputProps={{
+                                style: { color: 'black' },
+                            }}
+                            InputLabelProps={{
+                                style: { color: 'grey' },
+                            }}
+                        />
+                        <Button
+                            type="button"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={register}
+                        >
+                            Register
+                        </Button>
+                    </Box>
+                </Box>
+            </Paper>
+        </Container>
     );
 }
