41e940cbb71a531a7b1eae07a23b13b407128f6c
zhelyazkovse <zhelyazkovse@gmail.com>
2024-05-29 14:41:34 +0300

    adding storage to firebase

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +22,4 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 // the Realtime Database handler
 export const db = getDatabase(app);
+export const storage = getStorage(app);
