2a4081179030bfd5f84e0debc14b186299a213b6
zhelyazkovse <zhelyazkovse@gmail.com>
2024-06-08 18:56:41 +0200

    Add loader on initial load for pending auth

--- src/hoc/Authenticated.jsx
+++ src/hoc/Authenticated.jsx
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../config/firebase-config';
+import { CircularProgress } from '@mui/material';
 /**
  * 
  * @param {{children: any }} props 
@@ -11,9 +13,15 @@ import { AppContext } from '../context/AppContext';
 export default function Authenticated({ children }) {
     const { user } = useContext(AppContext);
     const location = useLocation();
+    const [,loading, ] = useAuthState(auth);
 
     if(!user) {
-        return <Navigate replace to="/login" state={{ from: location }}/>
+        if(loading) {
+            return <CircularProgress />
+        }
+        else {
+            return <Navigate replace to="/login" state={{ from: location }} />
+        }
     }
 
     return (
