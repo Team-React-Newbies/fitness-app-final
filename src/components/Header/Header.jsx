a1aa64e46118a55d530941bb0a78f4a7498c78f5
Ani Prodanovaa <aprodanova3@gmail.com>
2024-06-13 03:47:01 +0100

    add back lost NavLinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,59 +16,85 @@ export default function Header() {
     };
 
     return (
-        <div>
-            
-            <header className="header">
-                <div className="auth-links">
-                    {user ? (
-                        <>
-                             <NavLink to="/profile" id="username">
-                                {userData ? (
-                                    <Avatar
-                                        className="header-avatar"
-                                        src={userData.photoUrl}
-                                        sx={{ width: 40, height: 40 }}
-                                        title={userData.name || userData.handle} // Add the title attribute here
-                                    />
-                                ) : null}
-                            </NavLink>
+      <div>
+        <header className="header">
+          <div className="auth-links">
+            {user ? (
+              <>
+                <NavLink to="/profile" id="username">
+                  {userData ? (
+                    <Avatar
+                      className="header-avatar"
+                      src={userData.photoUrl}
+                      sx={{ width: 40, height: 40 }}
+                      title={userData.name || userData.handle} // Add the title attribute here
+                    />
+                  ) : null}
+                </NavLink>
 
-                            <Tooltip title="Must you leave us? 😢" arrow>
-                                <div>
-                            <Button onClick={logout} className="button-logout">Log Out</Button>
-                                </div>
-                            </Tooltip>
-                        </>
-                    ) : (
-                        <>
-                            <NavLink to="/login" className="auth-link">Login</NavLink>
-                            <NavLink to="/register" className="auth-link">Register</NavLink>
-                        </>
-                    )}
-                 </div>
-                <div className="top-nav">
-                    <nav className="nav-container">
-                        <Tooltip title="Sends you to Home page" arrow>
-                            <NavLink to="/" className="nav-link">Home</NavLink>
-                        </Tooltip>
-                        {user && userData && !userData.isBlocked && (
-                            <>
-                                <Tooltip title="Here you can create, edit and delete your exercises!" arrow>
-                                    <NavLink to="/exercises" className="nav-link">My Exercises</NavLink>
-                                </Tooltip>
-                                <Tooltip title="Tailor your own way of powering thru!" arrow>
-                                    <NavLink to="/goals" className="nav-link">My Goals</NavLink>
-                                </Tooltip>
-                            </>
-                        )}
-                        {userData && userData.isAdmin && (
-                            <Tooltip title="The Beheader" arrow>
-                                <NavLink to="/admin-dashboard" className="nav-link">Admin Dashboard</NavLink>
-                            </Tooltip>
-                        )}
-                    </nav>
-                </div>
-            </header>
-        </div>
+                <Tooltip title="Must you leave us? 😢" arrow>
+                  <div>
+                    <Button onClick={logout} className="button-logout">
+                      Log Out
+                    </Button>
+                  </div>
+                </Tooltip>
+              </>
+            ) : (
+              <>
+                <NavLink to="/login" className="auth-link">
+                  Login
+                </NavLink>
+                <NavLink to="/register" className="auth-link">
+                  Register
+                </NavLink>
+              </>
+            )}
+          </div>
+          <div className="top-nav">
+            <nav className="nav-container">
+              <Tooltip title="Sends you to Home page" arrow>
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </Tooltip>
+              {user && userData && !userData.isBlocked && (
+                <>
+                  <Tooltip title="Be Brave" arrow>
+                    <NavLink to="surprise-challenge" className="nav-link">
+                      Surprise Challenges
+                    </NavLink>
+                  </Tooltip>
+                  <Tooltip title="Coming Soon..." arrow>
+                    <NavLink to="exercise-videos" className="nav-link">
+                      Exercise Videos
+                    </NavLink>
+                  </Tooltip>
+                  <Tooltip
+                    title="Here you can create, edit and delete your exercises!"
+                    arrow
+                  >
+                    <NavLink to="/exercises" className="nav-link">
+                      My Exercises
+                    </NavLink>
+                  </Tooltip>
+                  <Tooltip title="Tailor your own way of powering thru!" arrow>
+                    <NavLink to="/goals" className="nav-link">
+                      My Goals
+                    </NavLink>
+                  </Tooltip>
+                </>
+              )}
+              {userData && userData.isAdmin && (
+                <Tooltip title="The Beheader" arrow>
+                  <NavLink to="/admin-dashboard" className="nav-link">
+                    Admin Dashboard
+                  </NavLink>
+                </Tooltip>
+              )}
+            </nav>
+          </div>
+        </header>
+      </div>
     );
 }
