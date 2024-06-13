2cc9e1eda8fd9e5a703aac92f4bf2a74b905a46e
Ani Prodanovaa <aprodanova3@gmail.com>
2024-06-13 05:16:24 +0100

    fix Exercise videos

--- src/views/ExerciseVideos.jsx
+++ src/views/ExerciseVideos.jsx
@@ -3,15 +3,6 @@ import { Box, Typography } from "@mui/material";
 
 export default function ExerciseVideos() {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      width="100vw"
-      textAlign="center"
-    >
-      <Typography variant="h2">Exercise Videos Coming Soon...</Typography>
-    </Box>
+    <h1>Exercise Videos Coming Soon...</h1>
   );
 }
